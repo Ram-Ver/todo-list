346cc025ac32e6c81e9294d41892f4846b36159c
Ram <ramv79863@gmail.com>
2023-04-12 21:55:01 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ToDo from "./ToDo";
+import React from 'react';
+import ToDo from './ToDo';
 
 function App() {
   return (
     <div className="App">
       <ToDo />
+      <h1>h1</h1>
     </div>
   );
 }
